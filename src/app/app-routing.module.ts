3e6ba3e8f5db300347efdf02eb4590e84ad5d011
Mati1432 <mateuszsliwinski9@gmail.com>
2023-07-18 11:48:36 +0200

    create home component and add route-outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path:'login', component:LoginComponent },
+  { path:'', component:HomeComponent },
+  { path:'home', component:HomeComponent },
 ];
 
 @NgModule({
