4ee6f9c461d22c1cd140a72b16df8f2decb68453
AndrzejStrz <andrzejstrzeszewski21@wp.pl>
2023-08-08 22:19:53 +0200

    animation on landing page

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,11 +1,28 @@
-import { Component, ViewEncapsulation  } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.css'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./landing-page.component.css']
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements OnInit{
+  title = 'landing page';
+  constructor(){}
+  ngOnInit(): void {}
 
-}
+  bgVariable:boolean = false;
+  headerVariable:boolean = false;
+
+  @HostListener('document:scroll')
+  onScroll(){
+      if(document.body.scrollTop > 60 || document.documentElement.scrollTop >60){
+    this.bgVariable = true;
+    this.headerVariable = true;
+  }
+  else{
+    this.bgVariable = false;
+    this.headerVariable = false;
+  }
+  }
+
+}  
