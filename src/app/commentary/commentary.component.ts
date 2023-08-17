c5c2e1662b45ec0adaf8dce0b24fce8d47edc158
Mateusz Śliwiński <mateuszsliwinski9@gmail.com>
2023-08-17 17:31:05 +0200

    add commentary filter

--- src/app/commentary/commentary.component.ts
+++ src/app/commentary/commentary.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { BoardService } from '../_services/board.services';
 
 @Component({
   selector: 'app-commentary',
@@ -8,12 +9,42 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class CommentaryComponent implements OnInit {
   postId!: string;
+  postDetails: any;
+  comments: any[];
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private boardService: BoardService) {
+    this.comments = [];
+  }
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       this.postId = params['postId'];
+      this.loadCommentsForPost(this.postId);
+      this.loadPost()
     });
   }
+
+  loadPost() {
+    this.boardService.getPostDetails(this.postId).subscribe(
+      postDetails => {
+        this.postDetails = postDetails;
+      },
+      error => {
+        console.error('Error loading post details:', error);
+      }
+    );
+  
+  }
+  loadCommentsForPost(postId: string) {
+    this.boardService.getCommentsForPost().subscribe(
+      comments => {
+        this.comments = comments.filter(comment => comment.post === postId);
+      },
+      error => {
+        console.error('Error loading comments:', error);
+      }
+    );
+  }
+
+
 }
